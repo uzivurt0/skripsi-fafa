634a308cd813dc58940a1f5d1bb092380204d914
Muhammad Faisal Farhan <gue.fafa00@gmail.com>
2022-11-21 13:52:17 +0700

    fixing frontend

--- src/pages/Forms/forms.js
+++ src/pages/Forms/forms.js
@@ -8,12 +8,12 @@ const Forms = () => {
     <div className="home-container">
       <div className="form-content-container">
         <div className="form-content">
-          <div className="home-content-item">
+          <div className="form-content-item">
             <div className="form-input">
               <label class="form-label" for="jenisMotor">
                 JENIS MOTOR
               </label>
-              <select id="jenisMotor">
+              <select className="form-selection" id="jenisMotor">
                 <option>Bebek</option>
                 <option>Matic</option>
                 <option>Sport</option>
@@ -23,7 +23,7 @@ const Forms = () => {
               <label class="form-label" for="jenisMotor">
                 UKURAN VELG
               </label>
-              <select id="jenisMotor">
+              <select className="form-selection" id="jenisMotor">
                 <option>10</option>
                 <option>12</option>
                 <option>14</option>
@@ -34,7 +34,7 @@ const Forms = () => {
               <label class="form-label" for="jenisMotor">
                 HARGA
               </label>
-              <select id="jenisMotor">
+              <select className="form-selection" id="jenisMotor">
                 <option>Kurang dari Rp.400.000</option>
                 <option>Rp.400.000 s/d Rp.600.000</option>
                 <option>Diatas Rp. 600.000</option>
@@ -44,7 +44,7 @@ const Forms = () => {
               <label class="form-label" for="jenisMotor">
                 PERUNTUKAN
               </label>
-              <select id="jenisMotor">
+              <select className="form-selection" id="jenisMotor">
                 <option>Harian</option>
                 <option>Semi-Balap</option>
                 <option>Balap</option>
@@ -54,7 +54,9 @@ const Forms = () => {
               <button className="home-button">Submit</button>
             </div>
           </div>
-          <img src={HomeImage} alt="Ban" />
+          <div className="form-image">
+            <img src={HomeImage} alt="Ban" />
+          </div>
         </div>
       </div>
     </div>
